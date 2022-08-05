97422c25db02f3478dcc0181832c151ca13ca559
Nate Duncan <natefduncan@gmail.com>
2022-08-05 15:49:27 -0500

    Add: More Opcodes

--- src/function.rs
+++ src/function.rs
@@ -7,15 +7,79 @@ pub trait Function {
 }
 
 #[derive(FromArgs)]
-pub struct Sum {
+pub struct Add {
     pub a: Value,  
     pub b: Value 
 }
 
-impl Function for Sum {
+impl Function for Add {
     fn evaluate(self) -> Result<Value, Error> {
         if self.a.is_num() && self.b.is_num() {
             Ok(Value::from(self.a.as_num() + self.b.as_num()))
+        } else {
+            Err(Error::Value)
+        }
+    }
+}
+
+#[derive(FromArgs)]
+pub struct Subtract {
+    pub a: Value, 
+    pub b: Value,
+}
+
+impl Function for Subtract {
+    fn evaluate(self) -> Result<Value, Error> {
+        if self.a.is_num() && self.b.is_num() {
+            Ok(Value::from(self.a.as_num() - self.b.as_num()))
+        } else {
+            Err(Error::Value)
+        }
+    }
+}
+
+#[derive(FromArgs)]
+pub struct Multiply {
+    pub a: Value,
+    pub b: Value, 
+}
+
+impl Function for Multiply {
+    fn evaluate(self) -> Result<Value, Error> {
+        if self.a.is_num() && self.b.is_num() {
+            Ok(Value::from(self.a.as_num() * self.b.as_num()))
+        } else {
+            Err(Error::Value)
+        }
+    }
+}
+
+#[derive(FromArgs)]
+pub struct Divide {
+    pub a: Value,
+    pub b: Value, 
+}
+
+impl Function for Divide {
+    fn evaluate(self) -> Result<Value, Error> {
+        if self.a.is_num() && self.b.is_num() {
+            Ok(Value::from(self.a.as_num() / self.b.as_num()))
+        } else {
+            Err(Error::Value)
+        }
+    }
+}
+
+#[derive(FromArgs)]
+pub struct Exponent {
+    pub a: Value,
+    pub b: Value, 
+}
+
+impl Function for Exponent {
+    fn evaluate(self) -> Result<Value, Error> {
+        if self.a.is_num() && self.b.is_num() {
+            Ok(Value::from(self.a.as_num().powf(self.b.as_num())))
         } else {
             Err(Error::Value)
         }
