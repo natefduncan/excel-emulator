38b0d1a9b802f6c21aba2776a16d58bfac461ba3
Nate Duncan <natefduncan@gmail.com>
2022-08-05 15:49:21 -0500

    Add: Ordering for Values

--- src/evaluate.rs
+++ src/evaluate.rs
@@ -1,24 +1,31 @@
 use time::Date; 
 use std::fmt; 
+use std::cmp::{Eq, PartialEq, PartialOrd, Ordering};  
 
 use crate::parse::{Expr, Opcode}; 
 use crate::function::*; 
 
-#[derive(Clone)]
+type NumType = f32;
+type BoolType = bool;
+type TextType = String; 
+type ArrayType = Vec<Value>;
+type DateType = Date; 
+
+#[derive(Clone, PartialEq)]
 pub enum Value { 
-    Num(f32), 
-    Bool(bool), 
-    Text(String), 
-    Date(Date), 
-    Array(Vec<Value>), 
+    Num(NumType), 
+    Bool(BoolType), 
+    Text(TextType), 
+    Date(DateType), 
+    Array(ArrayType), 
 }
 
-impl From<f32> for Value { fn from(f: f32) -> Value { Value::Num(f) }}
-impl From<bool> for Value { fn from(b: bool) -> Value { Value::Bool(b) }}
-impl From<String> for Value { fn from(s: String) -> Value { Value::Text(s) }}
+impl From<f32> for Value { fn from(f: NumType) -> Value { Value::Num(f) }}
+impl From<bool> for Value { fn from(b: BoolType) -> Value { Value::Bool(b) }}
+impl From<String> for Value { fn from(s: TextType) -> Value { Value::Text(s) }}
 impl From<&str> for Value { fn from(s: &str) -> Value { Value::Text(s.to_string()) }}
-impl From<Vec<Value>> for Value { fn from(v: Vec<Value>) -> Value { Value::Array(v) }}
-impl From<Date> for Value { fn from(d: Date) -> Value { Value::Date(d) }}
+impl From<Vec<Value>> for Value { fn from(v: ArrayType) -> Value { Value::Array(v) }}
+impl From<Date> for Value { fn from(d: DateType) -> Value { Value::Date(d) }}
 impl From<Expr> for Value {
     fn from(expr: Expr) -> Value {
         match expr {
@@ -28,14 +35,18 @@ impl From<Expr> for Value {
             Expr::Array(x) => Value::from(x.to_vec()), 
             Expr::Op(a, opcode, b) => {
                 match opcode {
-                    Opcode::Add => Sum {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
+                    Opcode::Add => Add {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
+                    Opcode::Subtract => Subtract {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
+                    Opcode::Multiply => Multiply {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
+                    Opcode::Divide => Divide {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
+                    Opcode::Exponent => Exponent {a: Value::from(*a), b: Value::from(*b)}.evaluate().unwrap(), 
                     _ => Value::from(-1.0) // TODO
                 }
             }, 
             Expr::Func {name, args } => {
                 match name.as_str() {
-                    "SUM" => Sum::from(args).evaluate().unwrap(), 
-                    _ => Value::from(-1.0)
+                    "SUM" => Add::from(args).evaluate().unwrap(), 
+                    _ => Value::from(-1.0) //TODO
                 }
             }, 
             _ => Value::from(-1.0) // TODO
@@ -61,7 +72,7 @@ impl Value {
     pub fn is_date(&self) -> bool { matches!(self, Value::Date(_)) }
     pub fn is_array(&self) -> bool { matches!(self, Value::Array(_)) }
 
-    pub fn as_num(&self) -> f32 {
+    pub fn as_num(&self) -> NumType {
         if let Value::Num(x) = self {
             *x
         } else {
@@ -69,7 +80,7 @@ impl Value {
         }
     }
 
-    pub fn as_bool(&self) -> bool {
+    pub fn as_bool(&self) -> BoolType {
         if let Value::Bool(x) = self {
             *x
         } else {
@@ -77,7 +88,7 @@ impl Value {
         }
     }
 
-    pub fn as_text(&self) -> String {
+    pub fn as_text(&self) -> TextType {
         if let Value::Text(x) = self {
             x.clone()
         } else {
@@ -85,7 +96,7 @@ impl Value {
         }
     }
 
-    pub fn as_date(&self) -> Date {
+    pub fn as_date(&self) -> DateType {
         if let Value::Date(x) = self {
             *x
         } else {
@@ -93,7 +104,7 @@ impl Value {
         }
     }
 
-    pub fn as_array(&self) -> Vec<Value> {
+    pub fn as_array(&self) -> ArrayType {
         if let Value::Array(x) = self {
             x.to_vec()
         } else {
@@ -118,6 +129,53 @@ impl fmt::Display for Value {
     }
 }
 
+impl Eq for Value { }
+
+fn variant_ord(v : &Value) -> usize {
+    let variants : Vec<bool> = vec![
+        v.is_bool(),
+        v.is_text(),
+        v.is_num(),
+        v.is_date()
+    ];
+    let variant_len : usize = variants.len();
+    match variants.into_iter().position(|x| x) {
+        Some(u) => {
+            u
+        },
+        None => {
+            variant_len
+        }
+    }
+}
+
+impl PartialOrd for Value {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        let self_rank : usize = variant_ord(self);
+        let other_rank : usize = variant_ord(other);
+        match self_rank.cmp(&other_rank) {
+            Ordering::Greater => {
+                Some(Ordering::Greater)
+            },
+            Ordering::Less => {
+                Some(Ordering::Less)
+            },
+            Ordering::Equal => {
+                if self.is_bool() {
+                    Some(self.as_bool().cmp(&other.as_bool()))
+                } else if self.is_text() {
+                    Some(self.as_text().cmp(&other.as_text()))
+                } else if self.is_num() {
+                    self.as_num().partial_cmp(&other.as_num())
+                } else if self.is_date() {
+                    Some(self.as_date().cmp(&other.as_date()))
+                } else {
+                    None
+                }
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -132,8 +190,17 @@ mod tests {
     }
 
     #[test]
-    fn test_num() {
+    fn test_op_codes() {
         assert_eq!(&evaluate_expr(" 1 + 1 "), "2");
+        assert_eq!(&evaluate_expr(" 1 - 1 "), "0"); 
+        assert_eq!(&evaluate_expr(" 2 * 2 "), "4"); 
+        assert_eq!(&evaluate_expr(" 8 / 4 "), "2"); 
+        assert_eq!(&evaluate_expr(" 8^2 "), "64"); 
+    }
+
+    #[test]
+    fn test_conditionals() {
+        assert_eq!(&evaluate_expr(" 1=1 "), "TRUE"); 
     }
 
     #[test]
