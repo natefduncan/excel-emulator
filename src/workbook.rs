1ce4b2426799df4c0e4ee4520053e6b9435842c2
Nate Duncan <natefduncan@gmail.com>
2022-08-15 14:19:39 -0500

    Add: Load Styles

--- src/workbook.rs
+++ src/workbook.rs
@@ -1,12 +1,15 @@
-use zip::read::{ZipArchive, ZipFile}; 
-use std::fs::File; 
-use std::io::{BufReader}; 
-use quick_xml::Reader;
-use quick_xml::events::{Event, BytesText};
+use quick_xml::events::BytesStart;
+use zip::read::{ZipArchive, ZipFile};
+use std::fs::File;
+use std::io::BufReader; 
+use quick_xml::{
+    Reader, 
+    events::{
+        Event, BytesText, 
+        attributes::Attribute
+    }, 
+};
 use anyhow::Result; 
-// use serde::{Serialize, Deserialize}; 
-
-// pub type ReaderType = Reader::<BufReader<ZipFile<'a>>>; 
 pub type ZipType = ZipArchive<File>; 
 
 pub struct Book {
@@ -44,10 +47,11 @@ impl From<&str> for Book {
 }
 
 impl Book {
-    fn load(&mut self) {
+    fn load(&mut self) -> Result<()> {
         self.load_sheets(); 
-        self.load_shared_strings(); 
+        self.load_shared_strings()?; 
         self.load_styles(); 
+        Ok(())
     }
 
     fn load_shared_strings(&mut self) -> Result<()> {
@@ -75,30 +79,99 @@ impl Book {
        Ok(())
     }
 
-    fn load_styles(&mut self) { }
+    fn load_styles(&mut self) {
+        let mut buf = Vec::new();
+        if let Ok(f) = self.zip.by_name("xl/sharedStrings.xml") {
+            let mut reader: Reader<BufReader<ZipFile>> = Reader::<BufReader<ZipFile>>::from_reader(BufReader::new(f)); 
+            let mut is_cell_xfs: bool = false;
+            loop {
+                match reader.read_event(&mut buf) {
+                    Ok(Event::Start(ref e)) if e.name() == b"cellXfs" || e.name() == b"xf" => {
+                        match e.name() {
+                            b"cellXfs" => { is_cell_xfs = true; },
+                            b"xf" => {
+                                if is_cell_xfs {
+                                    self.styles.push(Book::decode_style(&reader, e)); 
+                                }
+                            },
+                            _ => {}
+                        }
+                    }, 
+                    Ok(Event::Empty(ref e)) if e.name() == b"xf" => {
+                        if is_cell_xfs {
+                            self.styles.push(Book::decode_style(&reader, e)); 
+                        }
+                    }, 
+                    _ => {}
+                }
+            }
+        }
+    }
+
     fn load_sheets(&mut self) { }
+    fn load_sheet(&mut self, sheet_name: &str) -> Result<()> {
+        Ok(())
+    }
 
     fn zip_from_path(path: &str) -> ZipType {
         let file: File = File::open(path).expect("Unable to find file"); 
         zip::ZipArchive::new(file).expect("Unable to create zip") 
     }
 
-    fn get_shared_string_by_index(&self, index: usize) -> &Box<String> {
-        if let Some(SharedString(s)) = self.shared_strings.get(index) {
-            s
-        } else {
-            panic!("Shared string table does not have index {}", index)
-        }
-    }
+    // fn get_shared_string_by_index(&self, index: usize) -> &Box<String> {
+        // if let Some(SharedString(s)) = self.shared_strings.get(index) {
+            // s
+        // } else {
+            // panic!("Shared string table does not have index {}", index)
+        // }
+    // }
 
     fn decode_text_event(reader: &Reader<BufReader<ZipFile>>, e: &BytesText) -> String {
         e.unescape_and_decode(reader).unwrap()
+    }
+
+    fn decode_attribute_usize(reader: &Reader<BufReader<ZipFile>>, a: Attribute) -> usize {
+        a.unescape_and_decode_value(reader)
+        .unwrap()
+        .parse::<usize>()
+        .unwrap() 
+    }
+
+    fn decode_style(reader: &Reader<BufReader<ZipFile>>, e: &BytesStart) -> Style {
+        let mut number_format_id : usize = 0; 
+        let mut apply_number_format: bool = false; 
+        for a in e.attributes() {
+            let a = a.unwrap(); 
+            match a.key {
+                b"numFmtId" => {
+                    number_format_id = Book::decode_attribute_usize(reader, a); 
+                }, 
+                b"applyNumberFormat" => {
+                    apply_number_format = Book::decode_attribute_usize(reader, a) != 0; 
+                }, 
+                _ => {}
+            }
+        }
+        Style { number_format_id, apply_number_format }
     }
 }
 
 pub struct Sheet(String); 
 pub struct SharedString(Box<String>); 
 pub struct Style {
-    number_format_id: u32, 
+    number_format_id: usize, 
     apply_number_format: bool 
+}
+
+impl Default for Style {
+    fn default() -> Style {
+        Style { number_format_id: 0, apply_number_format: false }
+    }
+}
+
+impl Style {
+    fn new() -> Style {
+        Default::default()
+    }
+
 }
