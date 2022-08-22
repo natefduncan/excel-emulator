f3e48a58d3ae5ab1adc7f90b8294351845a41981
Nate Duncan <natefduncan@gmail.com>
2022-08-22 14:19:13 -0500

    Add: excel_to_date in utils

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,5 @@
+use chrono::{NaiveDate, Duration}; 
+
 use crate::evaluate::*;
 use crate::parse::Expr;
 use crate::excel::ExprParser; 
@@ -7,6 +9,16 @@ pub fn evaluate_expr(expr_str: &str) -> String {
     let expr : Box<Expr> = ExprParser::new().parse(expr_str).unwrap(); 
     println!("{:?}", expr); 
     format!("{}", Value::from(expr)) 
+}
+
+pub fn excel_to_date(serial: f64)  -> NaiveDate {
+    let start_date = NaiveDate::from_ymd(1899, 12, 30); 
+    let mut duration = Duration::days(serial as i64); 
+    let frac = (serial - serial.floor()) as i64; 
+    if frac > 0 {
+        duration = duration.checked_add(&Duration::hours(24 * frac)).expect("Could not add two dates together"); 
+    }
+    start_date.checked_add_signed(duration).unwrap()
 }
 
 
