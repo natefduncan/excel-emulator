0a73aa256d1bc17374bb9fe620836c7a22cfe4d3
Nate Duncan <natefduncan@gmail.com>
2022-08-27 22:03:43 -0500

    Add: Adjust formula and tests

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,9 @@ use chrono::{NaiveDate, Duration};
 
 use crate::evaluate::*;
 use crate::parse::Expr;
+use crate::excel::*; 
 use crate::excel::ExprParser; 
+use crate::reference::Reference; 
 
 pub fn evaluate_expr(expr_str: &str) -> String {
     println!("{}", expr_str); 
@@ -21,4 +23,69 @@ pub fn excel_to_date(serial: f64)  -> NaiveDate {
     start_date.checked_add_signed(duration).unwrap()
 }
 
+pub fn adjust_formula(
+    base_reference: Reference,
+    current_reference: Reference,
+    formula_text: String,
+) -> String {
+    let row_offset: i32 = current_reference.row() as i32 - base_reference.row() as i32;
+    let column_offset: i32 = current_reference.column() as i32 - base_reference.column() as i32;
+	let mut expression: Box<Expr> = ExprParser::new().parse(&formula_text).unwrap(); 
+    adjust_expression(row_offset, column_offset, &mut expression); 
+	format!("{}", expression) 
+}
 
+pub fn adjust_expression(
+    row_offset: i32, 
+    column_offset: i32, 
+    expression: &mut Expr
+) {
+    match *expression {
+        Expr::Cell { sheet: _, ref mut reference } => {
+            let mut r = Reference::from(reference.to_string());
+			r.offset((row_offset, column_offset));
+			*reference = r.to_string(); 
+        }, 
+        Expr::Op(ref mut a, _, ref mut b) => {
+            adjust_expression(row_offset, column_offset, a); 
+            adjust_expression(row_offset, column_offset, b); 
+        }, 
+        Expr::Func { name: _, ref mut args } => {
+            for arg in args.iter_mut() {
+                adjust_expression(row_offset, column_offset, arg); 
+            } 
+        }, 
+        Expr::Array(ref mut arr) => {
+            for a in arr.iter_mut() {
+                adjust_expression(row_offset, column_offset, a); 
+            }
+        }
+        _ => {}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::reference::Reference; 
+    use crate::utils::adjust_formula; 
+    
+    #[test]
+    fn test_adjust_formula() {
+       let base_reference = Reference::from((1, 1));
+       let current_reference = Reference::from((2, 1));
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("Sheet1!A1")), &"Sheet1!A2"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("A1+A2")), &"(A2+A3)"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("SUM(A1+A2)")), &"SUM((A2+A3))"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("{A1, A2}")), &"{A2, A3}"); 
+    }
+
+    #[test]
+    fn test_adjust_formula_anchored() {
+       let base_reference = Reference::from((1, 1));
+       let current_reference = Reference::from((2, 2));
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("Sheet1!$A$1")), &"Sheet1!$A$1"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("$A$1+A2")), &"($A$1+B3)"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("SUM(A$1+A$2)")), &"SUM((B$1+B$2))"); 
+       assert_eq!(&adjust_formula(base_reference, current_reference, String::from("{A1, A2}")), &"{B2, B3}"); 
+    }
+}
