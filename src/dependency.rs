43f610fff375f2a56015202808293b5f604ab1f5
Nate Duncan <natefduncan@gmail.com>
2022-08-31 15:31:03 -0500

    Add: Display with Dot; formula ordering with tests

--- src/dependency.rs
+++ src/dependency.rs
@@ -1,7 +1,11 @@
-use petgraph::graphmap::DiGraphMap; 
+use petgraph::{
+    graphmap::DiGraphMap, 
+    algo::toposort, 
+    dot::{Dot, Config}
+}; 
 use std::{fmt, cmp::Ordering}; 
 
-#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+#[derive(Hash, PartialEq, Eq, Clone, Copy, Debug)]
 pub struct CellId {
     sheet: usize, 
     row: usize,
@@ -41,7 +45,7 @@ impl fmt::Display for CellId {
 }
 
 pub struct DependencyTree {
-    tree: DiGraphMap<CellId, ()>, 
+    tree: DiGraphMap<CellId, u8>, 
 }
 
 /*
@@ -69,12 +73,32 @@ impl DependencyTree {
         self.add_cell_if_missing(precedent);
         self.add_cell_if_missing(cell);
         if !self.tree.contains_edge(*precedent, *cell) {
-            self.tree.add_edge(*precedent, *cell, ()); 
+            self.tree.add_edge(*precedent, *cell, 0); 
         }
    } 
 
     pub fn is_precedent_of(&self, cell1: &CellId, cell2: &CellId) -> bool {
         self.tree.contains_edge(*cell1, *cell2)
+    }
+
+    pub fn is_dependent_of(&self, cell1: &CellId, cell2: &CellId) -> bool {
+        self.tree.contains_edge(*cell2, *cell1) 
+    } 
+
+    pub fn get_order(&self) -> Vec<CellId> {
+        match toposort(&self.tree, None) {
+            Ok(order) => {
+                order.into_iter().rev().collect::<Vec<CellId>>()
+            }, 
+            Err(e) => panic!("{:?}", e) 
+        } 
+
+    } 
+}
+
+impl fmt::Display for DependencyTree {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", Dot::with_config(&self.tree, &[Config::EdgeNoLabel]))
     }
 }
 
@@ -87,8 +111,25 @@ mod tests {
         let mut tree = DependencyTree::new(); 
         let a = CellId::from((0,0,0)); 
         let b = CellId::from((1,0,0)); 
+        let c = CellId::from((2,0,0)); 
         tree.add_precedent(&a, &b); // A must calculate before B 
-        assert!(tree.is_precedent_of(&a, &b)); 
+        tree.add_precedent(&c, &b); // C must calculate before B 
+        assert!(tree.is_dependent_of(&b, &a)); 
+        assert_eq!(tree.is_dependent_of(&a, &b), false); 
+    }
+
+    #[test]
+    fn test_order() {
+        let mut tree = DependencyTree::new(); 
+        let a = CellId::from((0,0,0)); 
+        let b = CellId::from((1,0,0)); 
+        let c = CellId::from((2,0,0)); 
+        tree.add_precedent(&a, &b); // A must calculate before B 
+        tree.add_precedent(&b, &c); // B must calculate before C 
+        let mut order: Vec<CellId> = tree.get_order(); 
+        assert_eq!(order.pop().unwrap(), a);
+        assert_eq!(order.pop().unwrap(), b);
+        assert_eq!(order.pop().unwrap(), c);
     }
 }
 
