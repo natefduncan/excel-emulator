9464085a055f693f7f61c702dcbb52b616043f61
natefduncan <natefduncan@gmail.com>
2022-09-01 22:40:18 -0500

    Add: Lexer

---
+++ src/lexer.rs
@@ -0,0 +1,158 @@
+use nom::*; 
+use std::iter::Enumerate;
+use std::ops::{Range, RangeFrom, RangeFull, RangeTo};
+
+#[derive(PartialEq, Debug, Clone)]
+pub enum Token {
+    //Literal
+    Number(f32),
+    Boolean(bool), 
+    Text(String), 
+    // Error
+    Null, 
+    Div, 
+    Value, 
+    Ref, 
+    Name, 
+    Num, 
+    NA, 
+    GettingData, 
+    // Symbols
+    Plus,
+    Minus,
+    Divide,
+    Multiply,
+    Exponent, 
+    Ampersand, 
+    Equal,
+	Exclamation, 
+    Comma,
+    Colon,
+    SemiColon,
+    LAngle,
+    RAngle, 
+    LParen,
+    RParen,
+    LBrace,
+    RBrace,
+    LBracket,
+    RBracket,
+}
+
+#[derive(Clone, Copy, PartialEq, Debug)]
+pub struct Tokens<'a> {
+    pub tok: &'a [Token], 
+    pub start: usize, 
+    pub end: usize, 
+}
+
+impl<'a> Tokens<'a> {
+    pub fn new(vec: &'a [Token]) -> Self {
+        Tokens {
+            tok: vec,
+            start: 0,
+            end: vec.len(),
+        }
+    }
+}
+
+impl<'a> InputLength for Tokens<'a> {
+    fn input_len(&self) -> usize {
+        self.tok.len()
+    }
+}
+
+impl<'a> InputTake for Tokens<'a> {
+    fn take(&self, count: usize) -> Self {
+        Tokens {
+            tok: &self.tok[0..count],
+            start: 0,
+            end: count,
+        }
+    }
+
+    fn take_split(&self, count: usize) -> (Self, Self) {
+        let (prefix, suffix) = self.tok.split_at(count);
+        let first = Tokens {
+            tok: prefix,
+            start: 0,
+            end: prefix.len(),
+        };
+        let second = Tokens {
+            tok: suffix,
+            start: 0,
+            end: suffix.len(),
+        };
+        (second, first)
+    }
+}
+
+impl InputLength for Token {
+    fn input_len(&self) -> usize {
+        1
+    }
+}
+
+impl<'a> Slice<Range<usize>> for Tokens<'a> {
+    fn slice(&self, range: Range<usize>) -> Self {
+        Tokens {
+            tok: self.tok.slice(range.clone()),
+            start: self.start + range.start,
+            end: self.start + range.end,
+        }
+    }
+}
+
+impl<'a> Slice<RangeTo<usize>> for Tokens<'a> {
+    fn slice(&self, range: RangeTo<usize>) -> Self {
+        self.slice(0..range.end)
+    }
+}
+
+impl<'a> Slice<RangeFrom<usize>> for Tokens<'a> {
+    #[inline]
+    fn slice(&self, range: RangeFrom<usize>) -> Self {
+        self.slice(range.start..self.end - self.start)
+    }
+}
+
+impl<'a> Slice<RangeFull> for Tokens<'a> {
+    #[inline]
+    fn slice(&self, _: RangeFull) -> Self {
+        Tokens {
+            tok: self.tok,
+            start: self.start,
+            end: self.end,
+        }
+    }
+}
+
+impl<'a> InputIter for Tokens<'a> {
+    type Item = &'a Token;
+    type Iter = Enumerate<::std::slice::Iter<'a, Token>>;
+    type IterElem = ::std::slice::Iter<'a, Token>;
+
+    #[inline]
+    fn iter_indices(&self) -> Enumerate<::std::slice::Iter<'a, Token>> {
+        self.tok.iter().enumerate()
+    }
+    #[inline]
+    fn iter_elements(&self) -> ::std::slice::Iter<'a, Token> {
+        self.tok.iter()
+    }
+    #[inline]
+    fn position<P>(&self, predicate: P) -> Option<usize>
+    where
+        P: Fn(Self::Item) -> bool,
+    {
+        self.tok.iter().position(predicate)
+    }
+    #[inline]
+    fn slice_index(&self, count: usize) -> Result<usize, Needed> {
+        if self.tok.len() >= count {
+            Ok(count)
+        } else {
+            Err(Needed::Unknown)
+        }
+    }
+}
