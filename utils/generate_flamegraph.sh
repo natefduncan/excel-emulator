02390d3f31c6ad50e2437d9b10d522dfb8234b05
Nathan Duncan <nate@dunc.email>
2023-11-28 14:45:38 -0500

    Refactor: Change perf

--- utils/generate_flamegraph.sh
+++ utils/generate_flamegraph.sh
@@ -1,4 +1,5 @@
 cargo build --release 
-rm -rf ./utils/flame.svg && cd ./utils && perf record --call-graph dwarf,16384 -e cpu-clock -F 997 ../target/release/excel ../assets/test3.xlsx calculate Summary\!A1 
-perf script | stackcollapse-perf.pl | stackcollapse-recursive.pl | c++filt | rust-unmangle | flamegraph.pl > ./flame.svg
-rm -rf ./perf.data
+#rm -rf ./utils/flame.svg && cd ./utils && perf record --call-graph dwarf,16384 -e cpu-clock -F 997 ../target/release/excel ../assets/test3.xlsx calculate Summary\!A1 
+#perf script | stackcollapse-perf.pl | stackcollapse-recursive.pl | c++filt | rust-unmangle | flamegraph.pl > ./flame.svg
+#rm -rf ./perf.data
+flamegraph -o my_flamegraph.svg -- ./target/release/excel ../assets/test3.xlsx calculate Summary\!A1
